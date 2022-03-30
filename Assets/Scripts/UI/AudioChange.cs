6f2d8dda973011ee5707b6fa213c454d2bf698bf
Ethan Wang <ywan2928@uwo.ca>
2022-03-30 19:06:06 -0400

    add in-game menu

---
+++ Assets/Scripts/UI/AudioChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioChange : MonoBehaviour
+{
+    public Button button;
+    public Sprite audioOn;
+    public Sprite audioOff;
+
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.image.sprite = audioOn;
+    }
+
+    public void SwitchAudioImage()
+    {
+        button.image.sprite = button.image.sprite == audioOn ? audioOff : audioOn;
+    }
+}
