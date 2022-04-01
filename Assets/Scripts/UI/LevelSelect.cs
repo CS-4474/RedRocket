7fd05ef8cb4d8d282f77893dc4546af590363903
jatan <jatan21@gmail.com>
2022-04-01 09:55:01 -0400

    Added Level Select

---
+++ Assets/Scripts/UI/LevelSelect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelect : MonoBehaviour
+{
+    private int maxLevel;
+
+    [SerializeField]
+    private List<GameObject> levelList;
+
+    void Start()
+    {
+        maxLevel = PlayerPrefs.GetInt("maxLevel", 0);
+
+        for (int i = 0; i <= maxLevel; i++)
+        {
+            levelList[i].SetActive(true);
+        }
+    }
+}
