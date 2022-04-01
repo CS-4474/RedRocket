cffc76b98e4d06a9ce6ca92323d2ba9b8301a013
jatan <jatan21@gmail.com>
2022-04-01 10:05:34 -0400

    Made Play continue where you left off

--- Assets/Scripts/UI/Level.cs
+++ Assets/Scripts/UI/Level.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class Level : MonoBehaviour
 {
     public int level;
+    private int maxLevel;
 
     public void SelectLevel()
     {
+        maxLevel = PlayerPrefs.GetInt("maxLevel", 0);
         PlayerPrefs.SetInt("SelectedLevel", level);
+    }
+
+    public void ContinueLevel()
+    {
+        PlayerPrefs.SetInt("SelectedLevel", maxLevel);
     }
 }
