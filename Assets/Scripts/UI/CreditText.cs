7bbf560ea770741d835979bbba0f8d1da326ce4e
Cameron David McCarthy <cmccar43@uwo.ca>
2022-04-03 14:27:24 -0400

    Removed small caps from credits for readability and credited original authors

--- Assets/Scripts/UI/CreditText.cs
+++ Assets/Scripts/UI/CreditText.cs
@@ -9,10 +9,10 @@ public class CreditText : MonoBehaviour
     private void Awake()
     {
         textGUI = GetComponent<TextMeshProUGUI>();
-        String text = "Creators\n Original Programmer & Designer \n Cameron McCarthy\n Evan Rome-Bailey\n Jakob Wanger\n Jatan Patel\n Kevin Gao\n Yuchen Wang\n\n";
-        text += "Used Assets\n Smoke and Fire Sprites\n Rocket Sound\n Bumping Sound\n Checkpoint Sound\n Splat Sound\n\n";
-        text += "White Lotus Kevin MacLeod (incompetech.com) Licensed under Creative Commons: By Attribution 3.0 License http://creativecommons.org/licenses/by/3.0/ \n";
-        text += "Made for the Bournemouth University 48 hour game jam.";
+        String text = "Creators\n\n Original Programmer & Original Designer \n Laura Jenkins & Clara Kane\n\n CS4474 Redesign Team\n Cameron McCarthy\n Evan Rome-Bailey\n Jakob Wanger\n Jatan Patel\n Kevin Gao\n Yuchen Wang\n\n\n";
+        text += "Used Assets\n\n Smoke and Fire Sprites\n Rocket Sound\n Bumping Sound\n Checkpoint Sound\n Splat Sound\n\n";
+        text += "White Lotus by Kevin MacLeod (incompetech.com)\n Licensed under Creative Commons: By Attribution 3.0 License http://creativecommons.org/licenses/by/3.0/ \n\n";
+        text += "Originally made for the Bournemouth University 48 hour game jam.";
         textGUI.text = text;
     }
 }
