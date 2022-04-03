a91b9f90cfdd573ac0c73d24282b9b0fbf18379a
Ethan Wang <ywan2928@uwo.ca>
2022-04-03 17:56:38 -0400

    update instruction

---
+++ Assets/Scripts/UI/InstructionText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using TMPro;
+using System.Collections.Generic;
+using System;
+public class InstructionText : MonoBehaviour
+{
+    TextMeshProUGUI textGUI;
+
+    private void Awake()
+    {
+        textGUI = GetComponent<TextMeshProUGUI>();
+        String text = "Instruction\n\n";
+        text += "The goal of this game is to move your rocket to the next checkpoint. To view the map, you can use mouse scrollwheel to zoom in and out.\n";
+        textGUI.text = text;
+    }
+}
+
